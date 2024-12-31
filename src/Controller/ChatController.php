50fcb1b3243c256f79b74d21ddef1d2b0f6839cc
innermost47 <anthony.charretier@gmail.com>
2024-12-31 11:41:06 +0100

    get after current time messages

--- src/Controller/ChatController.php
+++ src/Controller/ChatController.php
@@ -177,7 +177,7 @@ class ChatController extends AbstractController
                 return $this->json([], 200);
             }
             try {
-                $afterDate = new \DateTimeImmutable('+1 second');
+                $afterDate = new \DateTimeImmutable();
                 error_log('After timestamp: ' . $afterDate->format('Y-m-d H:i:s'));
                 $messages = $this->messageRepository->findNewMessages(
                     $channel,
